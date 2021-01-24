d477f2ab44d59d1e89f79466f5fe892e4505192e
Raul Rodriguez <raulrobi@gmail.com>
2021-01-23 21:06:00 -0300

    Class 173 - History push/replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-export const LoginScreen = () => {
-    return (
-        <div>
-            <h1>LoginScreen</h1>
-        </div>
-    );
+// Aquí extraemos la propiedad history que ya existe en el componente Route
+// ya que es agregado por React
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    // history.push("/");
+    history.replace("/");
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
 };

8c9944108d06311c45e142b06994d044d3764605
Raul Rodriguez <raulrobi@gmail.com>
2021-01-26 09:20:16 -0300

    Class 175 - Hero card

--- src/components/routers/DashboardRoutes.js
+++ src/components/routers/DashboardRoutes.js
@@ -1,24 +1,28 @@
 import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
-import { MarvelScreen } from "../marvel/MarvelScreen";
-import { DcScreen } from "../dc/DcScreen";
-import { Navbar } from "../ui/Navbar";
-import { HeroScreen } from "../heroes/HeroScreen";
+import {Redirect, Route, Switch} from "react-router-dom";
+import {MarvelScreen} from "../marvel/MarvelScreen";
+import {DcScreen} from "../dc/DcScreen";
+import {Navbar} from "../ui/Navbar";
+import {HeroScreen} from "../heroes/HeroScreen";
 
 export const DashboardRoutes = () => {
-  return (
-    <>
-      <Navbar />
-      <div className="card-columns">
-        <Switch>
-          <Route exact path="/marvel" component={MarvelScreen} />
-          <Route exact path="/heroes/:heroeId" component={HeroScreen} />
-          <Route exact path="/dc" component={DcScreen} />
+    return (
+        <>
+            <Navbar />
+            <div className="container mt-2">
+                <Switch>
+                    <Route exact path="/marvel" component={MarvelScreen} />
+                    <Route
+                        exact
+                        path="/heroes/:heroeId"
+                        component={HeroScreen}
+                    />
+                    <Route exact path="/dc" component={DcScreen} />
 
-          {/* Si no encuentra ningún path, lo redirige a marvel */}
-          <Redirect to="/marvel" />
-        </Switch>
-      </div>
-    </>
-  );
+                    {/* Si no encuentra ningún path, lo redirige a marvel */}
+                    <Redirect to="/marvel" />
+                </Switch>
+            </div>
+        </>
+    );
 };

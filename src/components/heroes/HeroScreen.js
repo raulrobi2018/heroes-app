a323a157a580deab7aabd1c123600cc26e6b7428
Raul Rodriguez <raulrobi@gmail.com>
2021-01-28 10:03:21 -0300

    Class 177 - Styling HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,9 @@ import React from "react";
 import {Redirect, useParams} from "react-router-dom";
 import {getHeroById} from "../../selectors/getHeroById";
 
-export const HeroScreen = () => {
+// Aquí extraemos la propiedad history que ya existe en el componente Route
+// ya que es agregado por React
+export const HeroScreen = ({history}) => {
     // Este hook de React va a extraer los parametros que viajen en la url
     const {heroId} = useParams();
 
@@ -11,6 +13,16 @@ export const HeroScreen = () => {
     if (!hero) {
         return <Redirect to="/" />;
     }
+
+    const handleBack = () => {
+        // Si el history solo tiene 2 páginas en el historial, se redirecciona
+        //al inicio. Esto se puede dar si copian la url y la pegan en un navegador
+        if (history.length <= 2) {
+            history.push("/");
+        } else {
+            history.goBack();
+        }
+    };
 
     const {
         superhero,
@@ -21,8 +33,38 @@ export const HeroScreen = () => {
     } = hero;
 
     return (
-        <div>
-            <h1>HeroScreen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img
+                    src={`../assets/heroes/${heroId}.jpg`}
+                    alt={superhero}
+                    className="img-thumbnail"
+                />
+            </div>
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <strong>Alter ego: </strong>
+                        {alter_ego}
+                    </li>
+                    <li className="list-group-item">
+                        <strong>Publisher: </strong>
+                        {publisher}
+                    </li>
+                    <li className="list-group-item">
+                        <strong>First appearance: </strong>
+                        {first_appearance}
+                    </li>
+                </ul>
+
+                <h5>Characters</h5>
+                <p>{characters}</p>
+
+                <button className="btn btn-outline-info" onClick={handleBack}>
+                    {"< Back"}
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">
-                Asociaciones
+                Heroes
             </Link>
 
             <div className="navbar-collapse">

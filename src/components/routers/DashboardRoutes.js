2227e240c0c24cff41bbeb768b92d4d15220eb9a
Raul Rodriguez <raulrobi@gmail.com>
2021-01-30 20:36:57 -0300

    Class 180 - SearchScreen component

--- src/components/routers/DashboardRoutes.js
+++ src/components/routers/DashboardRoutes.js
@@ -1,24 +1,26 @@
 import React from "react";
-import {Redirect, Route, Switch} from "react-router-dom";
-import {MarvelScreen} from "../marvel/MarvelScreen";
-import {DcScreen} from "../dc/DcScreen";
-import {Navbar} from "../ui/Navbar";
-import {HeroScreen} from "../heroes/HeroScreen";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { MarvelScreen } from "../marvel/MarvelScreen";
+import { DcScreen } from "../dc/DcScreen";
+import { Navbar } from "../ui/Navbar";
+import { HeroScreen } from "../heroes/HeroScreen";
+import { SearchScreen } from "../search/SearchScreen";
 
 export const DashboardRoutes = () => {
-    return (
-        <>
-            <Navbar />
-            <div className="container mt-2">
-                <Switch>
-                    <Route exact path="/marvel" component={MarvelScreen} />
-                    <Route exact path="/hero/:heroId" component={HeroScreen} />
-                    <Route exact path="/dc" component={DcScreen} />
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/marvel" component={MarvelScreen} />
+          <Route exact path="/heroes/:heroeId" component={HeroScreen} />
+          <Route exact path="/dc" component={DcScreen} />
+          <Route exact path="/search" component={SearchScreen} />
 
-                    {/* Si no encuentra ningún path, lo redirige a marvel */}
-                    <Redirect to="/marvel" />
-                </Switch>
-            </div>
-        </>
-    );
+          {/* Si no encuentra ningún path, lo redirige a marvel */}
+          <Redirect to="/marvel" />
+        </Switch>
+      </div>
+    </>
+  );
 };
--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,29 +1,58 @@
 import React from "react";
+import { heroes } from "../../data/heroes";
+import { useForm } from "../../hooks/useForm";
+import { HeroCard } from "../heroes/HeroCard";
 
 export const SearchScreen = () => {
-    return (
-        <div>
-            <h1>Search</h1>
-            <hr />
+  const heroesFiltered = heroes;
 
-            <div className="row">
-                <div className="col-5">
-                    <h4>Search form</h4>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Find a hero..."
-                            className="form-control"
-                        />
-                    </form>
-                    <button
-                        type="submit"
-                        className="btn btn-block btn-outline-primary m-1"
-                    ></button>
-                </div>
-                <div></div>
-                <div></div>
-            </div>
+  const [formValues, { handleInputChange }] = useForm({
+    searchText: ""
+  });
+
+  //   Desestructuro searchText de formValues
+  const { searchText } = formValues;
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(searchText);
+  };
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Search form</h4>
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Find a hero..."
+              className="form-control"
+              name="searchText"
+              value={searchText}
+              onChange={handleInputChange}
+              autoComplete="off"
+            />
+            <button
+              type="submit"
+              className="btn btn-block btn-outline-primary m-1"
+            >
+              Search
+            </button>
+          </form>
         </div>
-    );
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          {heroesFiltered.map((h) => (
+            <HeroCard key={h.id} {...h} />
+          ))}
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };

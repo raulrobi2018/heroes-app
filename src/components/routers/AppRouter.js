5217d15737c5546794d5b3d3cd5225b827299a67
Raul Rodriguez <raulrobi@gmail.com>
2021-02-03 09:36:15 -0300

    Class 191 - Private Routes

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {AuthContext} from "../../auth/AuthContext";
 import {LoginScreen} from "../login/LoginScreen";
 import {DashboardRoutes} from "./DashboardRoutes";
+import {PrivateRoute} from "./PrivateRoute";
 
 export const AppRouter = () => {
+    //Leemos el usuario del contexto para saber si está autenticado
+    const {user} = useContext(AuthContext);
+
     return (
         <Router>
             <div className="container">
@@ -12,7 +17,11 @@ export const AppRouter = () => {
                 <Switch>
                     {/* exact para que machee la ruta exacta */}
                     <Route exact path="/login" component={LoginScreen} />
-                    <Route path="/" component={DashboardRoutes} />
+                    <PrivateRoute
+                        path="/"
+                        component={DashboardRoutes}
+                        isAutenticated={user.logged}
+                    />
                 </Switch>
             </div>
         </Router>
